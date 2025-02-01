e5cf3b884ea0e27c62966d5e6219fbc62ed50412
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-01 10:47:28 -0500

    Update arrayToObject.js

--- Array/arrayToObject.js
+++ Array/arrayToObject.js
@@ -1,3 +1,11 @@
 function arrayToObject(arr){
+    let seen={};
+    for(let i=0;i<arr.length;i++){
+        if(seen[arr[i]]){
+            return arr[i];
+        }
+        seen[arr[i]]=true;
+    }
+    return -1;
 
 }
