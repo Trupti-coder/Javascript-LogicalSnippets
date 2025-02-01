d3852dd2ae06ad8b0318c068a6452de6b7463241
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-01-31 22:55:12 -0500

    Update allUniqueEleArray.js

---
+++ Array/allUniqueEleArray.js
@@ -0,0 +1,13 @@
+function allUnique(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[i] === arr[j]) {
+                return false; // Duplicate found
+            }
+        }
+    }
+    return true; // No duplicates
+}
+
+
+
