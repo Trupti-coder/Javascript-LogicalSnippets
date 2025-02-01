3f834e35ad5ab312f4f6d8ba434a501bd86a5722
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-01 10:42:22 -0500

    Update sortArray.js

--- Array/arraySort.js
+++ Array/arraySort.js
@@ -1,6 +1,6 @@
 function sortArray(arr){
     for(let i=0;i<arr.length-1;i++){
-        for(let j=0;j<arr.length-1-i;i++){
+        for(let j=0;j<arr.length-1-i;j++){
             if(arr[j]>arr[j+1]){
                 //swap elements
                 let temp=arr[j];
@@ -12,3 +12,7 @@ function sortArray(arr){
     return arr;
 
 }
+
+//Example usage
+let array=[1,4,6,8,3,4];
+console.log(sortArray(array));
