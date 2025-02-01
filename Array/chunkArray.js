1ca7f43a18995a6c04a4d4f883d0bfb626bd7746
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-01 10:52:53 -0500

    Update chunkArray.js

--- Array/chunkArray.js
+++ Array/chunkArray.js
@@ -1,3 +1,16 @@
 function arrayChunk(arr,k){
+   let result=[];
+   let chunk=[];
+   for(let i=onabort;i<arr.length;i++){
+    chunk.push(arr[i]);
+   }
+   if(chunk.length===k){
+    result.push(chunk);
     
+   } 
+
+   if(chunk.length>0){
+    result.push(chunk);
+   }
+   return result;
 }
