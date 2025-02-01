cdee8ce2bf971fd1fff3f83d60000c7c51af5a35
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-01 10:34:14 -0500

    Update arraySort.js

--- Array/arraySort.js
+++ Array/arraySort.js
@@ -1,3 +1,14 @@
-function sortArray(){
-    
+function sortArray(arr){
+    for(let i=0;i<arr.length-1;i++){
+        for(let j=0;j<arr.length-1-i;i++){
+            if(arr[j]>arr[j+1]){
+                //swap elements
+                let temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
+    }
+    return arr;
+
 }
