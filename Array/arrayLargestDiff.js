4305864cbd0f257f7115bfaf51f62017e55928c3
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-01-31 23:01:22 -0500

    Update arrayLargestDiff.js

--- Array/arrayLargestDiff.js
+++ Array/arrayLargestDiff.js
@@ -1,3 +1,19 @@
 function largestDifference(arr){
-    
+    if (arr.length < 2) {
+        return 0; // If there are fewer than 2 elements, there's no meaningful difference
+    }
+
+    let min = arr[0];
+    let max = arr[0];
+
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] < min) {
+            min = arr[i];
+        } else if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+
+    return max - min;
+
 }
