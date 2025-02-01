6f408c6283021be88105a083cfe907f165645e9f
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-01 09:44:39 -0500

    Update arrayReverse.js

--- Array/arrayReverse.js
+++ Array/arrayReverse.js
@@ -2,7 +2,15 @@ function reverseArray(array){
     let start=0;
     let end=arr.length-1;
     while(start<end){
-        
+        //swap elements at start and end 
+        var temp=array[start];
+        array[start]=array[end];
+        array[end]=temp;
+
+        //Move towards the middle
+        start++;
+        end--;
     }
+    return array;
 
 }
