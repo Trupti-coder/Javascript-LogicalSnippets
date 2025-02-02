08e56f1d11869a572505513a3dba1541eb52e858
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-02 12:52:33 -0500

    Update findSecondSmallest.js

--- Array/findSecondSmallest.js
+++ Array/findSecondSmallest.js
@@ -1,3 +1,17 @@
 function findSecondSmallest(arr){
+    if(arr.length<2){
+        return false;
+    }
+
+    let smallest=Infinity;
+    let secondSmallest=Infinity;
+
+    for(let i=0;i<arr.length;i++){
+        if(arr[i]<smallest){
+            secondSmallest=smallest;
+            smallest=arr[i];
+        }
+
+    }
 
 }
