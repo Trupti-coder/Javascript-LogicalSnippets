0e2660c5df6a9b71a49559f6214ecb97e830cbe4
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-02 14:07:49 -0500

    Update majorElementArrayOccure.js

---
+++ Array/majorElementArrayOccure.js
@@ -0,0 +1,16 @@
+function findMajorElement(arr){
+    let maxCount=0; 
+    let majorityElement=null;
+
+    for(let i=0;i<arr.length;i++){
+        let count=0;
+    }
+
+    for(let j=0;j<arr.length;j++){
+        if(arr[i]===arr[j]){
+            count++;
+        }
+    }
+
+
+}
