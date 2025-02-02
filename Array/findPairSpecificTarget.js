da5c289cb959fcb5d45b77918090b459cb3b3fcc
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-02 12:46:41 -0500

    Update findPairSpecificTarget.js

--- Array/findPairSpecificTarget.js
+++ Array/findPairSpecificTarget.js
@@ -1,13 +1,16 @@
-function findPairs(arr,k){
+function findPairs(arr,target){
     let pairs=[];
     for(let i=0;i<arr.length;i++){
         for(let j=i+1;j<arr.length;i++){
-            if(arr[i]+arr[j]===k){
+            if(arr[i]+arr[j]===target){
                 pairs.push([arr[i],arr[j]]);
             }
         }
     }
     return pairs;
-
-
 }
+
+//Example usage
+let arr=[1,2,3,4,5,6,7,8,9,0];
+let target=18;
+console.log(findPairs(arr,target));
