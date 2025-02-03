0d696cca860937e850f5ce61214955b33ca2f7f6
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-03 18:35:07 -0500

    Update maxSubArray.js

--- Array/maxSubArray.js
+++ Array/maxSubArray.js
@@ -5,4 +5,13 @@ function maxSubArray(arr){
 
     let maxSum=arr[0];
     let currentSum=arr[0];
+
+    for(let i=1;i<arr.length;i++){
+        if(arr[i]>currentSum+arr[i]){
+            currentSum=arr[i];
+        }
+        else{
+            currentSum=currentSum+arr[i];
+        }
+    }
 }
