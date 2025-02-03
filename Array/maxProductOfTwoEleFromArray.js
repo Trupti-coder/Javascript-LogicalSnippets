73d6a2539938b9216b2a29a6a807cee540493875
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-03 18:28:22 -0500

    Update maxProductOfTwoEleFromArray.js

--- Array/maxProductOfTwoEleFromArray.js
+++ Array/maxProductOfTwoEleFromArray.js
@@ -2,7 +2,11 @@ function maxProduct(arr){
     let max=Infinity;
     for(let i=0;i<arr.length;i++){
         for(let j=i+1;j<arr.length;j++){
-            let product=arr[i]*arr[j]
+            let product=arr[i]*arr[j];
+            if(product>max){
+                max=product;
+            }
         }
     }
+    return max;
 }
