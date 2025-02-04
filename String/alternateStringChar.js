821b02d9f325bd8e617cd281be3527550c3fe1dd
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-04 10:29:18 -0500

    Update alternateStringChar.js

--- String/alternateStringChar.js
+++ String/alternateStringChar.js
@@ -4,6 +4,14 @@ function alternateString(s1,s2){
 
     //loop through both strings up to the length of the longer string
     for(let i=0;i<maxLength;i++){
-        
+        if(i<s1.length){
+            let code1=s1.charCodeAt(i);
+            result+=String.fromCharCode(code1);
+        }
+        if(i<s2.length){
+            let code2=s2.charCodeAt(i);
+            result+=String.fromCharCode(code2);
+        }
     }
+    return result;
 }
