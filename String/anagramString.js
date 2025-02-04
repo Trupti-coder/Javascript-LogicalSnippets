2ac8a96c78ef0c0bc2ecd139972482369fefc5ed
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-04 12:48:09 -0500

    Update areAnagrams.js

--- String/anagramString.js
+++ String/anagramString.js
@@ -2,5 +2,15 @@ function areAnagrams(str1,str2){
     if(str1.length !==str2.length){
         return false;
     }
+    function sortString(){
+        let arr=str1.split('');
+        for(let i=0;i<arr.length;i++){
+            for(let j=i+1;j<arr.length;j++){
+                if(arr[i]>arr[j]){
+                    
+                }
+            }
+        }
+    }
 
 }
