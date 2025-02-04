ffa70f32dad165434117c7d06a7f59b9cb0f06a2
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-04 12:52:07 -0500

    Update anagramString.js

--- String/anagramString.js
+++ String/anagramString.js
@@ -7,10 +7,17 @@ function areAnagrams(str1,str2){
         for(let i=0;i<arr.length;i++){
             for(let j=i+1;j<arr.length;j++){
                 if(arr[i]>arr[j]){
-                    
+                    let temp=arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=temp;
                 }
             }
         }
+        return arr.join('');
     }
+
+    let sortedStr1=sortString(str1);
+    let sortedStr2=sortString(str2);
+    return sortedStr1===sortedStr2;
 
 }
