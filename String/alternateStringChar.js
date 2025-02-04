955a3ee9b01076aee1725544dc8e530dcb12c477
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-04 10:26:36 -0500

    Update alternateStringChar.js

---
+++ String/alternateStringChar.js
@@ -0,0 +1,9 @@
+function alternateString(s1,s2){
+    let result='';
+    let maxLength=s1.length>s2.length?s1.length:s2.length;
+
+    //loop through both strings up to the length of the longer string
+    for(let i=0;i<maxLength;i++){
+        
+    }
+}
