c4f656c866da6b33e86bdd9644a6edaeca591b72
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-04 13:02:41 -0500

    Update compaireStringLength.js

--- String/compaireStringLength.js
+++ String/compaireStringLength.js
@@ -1,3 +1,17 @@
-function compaireString(){
-    
+function compaireString(str1,str2){
+    let len1=str1.length;
+    let len2=str2.length;
+
+    if(len1===len2){
+        return "Equal"
+    }
+    else if(){
+
+    }
+    else{
+        
+    }
+
+
+
 }
