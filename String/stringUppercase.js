8855ada1f819ce7c2752f4cede6c63092ae3f4ff
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-05 11:41:15 -0500

    Update stringUppercase.js

--- String/stringUppercase.js
+++ String/stringUppercase.js
@@ -1,3 +1,13 @@
 function toUppercase(str){
-
+    let upperStr='';
+    for(let i=0;i<str.length;i++){
+        let charCode=str.charCodeAt(i);
+        if(charCode>=97 && charCode<=122){
+            upperStr+=String.fromCharCode(charCode-32);
+        }
+        else{
+            lowerStr+=str[i];
+        }
+    }
+    return upperStr;
 }
