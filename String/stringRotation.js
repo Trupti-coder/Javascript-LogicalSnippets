a295a532e687da02ff645d69030299e65d0ab9e4
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-05 11:57:15 -0500

    Update stringRotation.js

--- String/stringRotation.js
+++ String/stringRotation.js
@@ -1,3 +1,19 @@
-function isRotation(){
+function isRotation(s1,s2){
+     // Check if lengths are the same; if not, s2 can't be a rotation of s1
+     if (s1.length !== s2.length) {
+        return false;
+    }
+
+    // Concatenate s1 with itself
+    let concatenated = '';
+    for (let i = 0; i < s1.length; i++) {
+        concatenated += s1[i]; // Build concatenated string
+    }
+    for (let i = 0; i < s1.length; i++) {
+        concatenated += s1[i]; // Add s1 again to itself
+    }
+
+
     
+
 }
