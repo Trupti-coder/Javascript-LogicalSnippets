e23594a8fd1b718101669efe01e91423a8643e94
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-04 22:20:13 -0500

    Update compressString.js

--- String/compressString.js
+++ String/compressString.js
@@ -1,3 +1,14 @@
 function compressString(){
-    
+    let compressed='';
+    let count=1;
+    for(let i=0;i<structuredClone.length;i++){
+        if(str[i]===str[i+1]){
+            count++;
+        }
+        else{
+            compressed+=str[i]+count;
+            count=1;
+        }
+    }
+    return compressed;
 }
