0f0819ed8015a51563726f7da0fd4fb1b6f93df8
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-05 09:44:27 -0500

    Update stringVowelFind.js

--- String/stringVowelFind.js
+++ String/stringVowelFind.js
@@ -1,6 +1,14 @@
 function findVowel(str){
     var vowel=["a","e","i","o","u","A","E","I","O","U"];
+   let foundVowel="";
     for(let i=0;i<str.length;i++){
-        
+        for(let j=0;j<vowel.length;j++){
+            if(str[i]===vowel[j]){
+                foundVowel+=str[i];
+                break;
+
+            }
+        }
     }
+    return foundVowel;
 }
