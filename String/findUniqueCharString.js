8f19a786426c3eacaac951256846c921845c3b3b
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-05 09:18:09 -0500

    Update findUniqueCharString.js

--- String/findUniqueCharString.js
+++ String/findUniqueCharString.js
@@ -1,3 +1,11 @@
 function hasUniqueChar(){
-    
+    let seenChars={};
+    for(let i=0;i<str.length;i++){
+        let char=str[i];
+
+        if(seenChars[char]){
+            return false;
+        }
+        seenChars[char]=true;
+    }
 }
