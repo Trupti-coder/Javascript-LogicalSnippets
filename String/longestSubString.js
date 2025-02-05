4ca0f05e649a490de4ffbd57d11bbdd11807c198
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-05 09:25:52 -0500

    Update longestSubString.js

--- String/longestSubString.js
+++ String/longestSubString.js
@@ -1,3 +1,16 @@
 function longestSubstringWithoutRepeating(s){
-    
+    let maxLength=0;
+    let start=0;
+    let seen={};
+
+    for(let end=0;end<s.length;end++){
+        let char=s[end];
+
+        if(seen[char]!==undefined && seen[char]>=start){
+            start=seen[char]+1;
+        }
+        seen[char]=end;
+        maxLength=Math.max(maxLength,end-start+1);
+    }
+    return maxLength;
 }
