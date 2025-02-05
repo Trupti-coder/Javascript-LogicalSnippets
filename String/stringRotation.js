3ef23a12bbc323fa2bbb3d9856b31a19f04fd4b3
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-05 11:57:51 -0500

    Update stringRotation.js

--- String/stringRotation.js
+++ String/stringRotation.js
@@ -14,6 +14,20 @@ function isRotation(s1,s2){
     }
 
 
-    
+     // Check if s2 is a substring of concatenated
+     for (let i = 0; i <= concatenated.length - s2.length; i++) {
+        let match = true;
+        for (let j = 0; j < s2.length; j++) {
+            if (concatenated[i + j] !== s2[j]) {
+                match = false;
+                break;
+            }
+        }
+        if (match) {
+            return true; // Found s2 as a substring
+        }
+    }
+
+    return false; // s2 is not a rotation of s1
 
 }
