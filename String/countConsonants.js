1efe35516e52cb77d733aa6afa16ac0f04b2ef39
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-04 23:00:03 -0500

    Update countConsonants.js

--- String/countConsonants.js
+++ String/countConsonants.js
@@ -4,12 +4,20 @@ function countConsonants(){
 
     for(let i=0;i<str.length;i++){
         let char=str[i];
+
+        if((char>="a" && char<="z")|| (char>="A" && char<="Z")){
+            let isVowel=false;
         for(let j=0;j<vowels.length;j++){
             if(char===vowels[j]){
                 isVowel=true;
                 break;
             }
         }
+        if(!isVowel){
+            countConsonants++;
+        }
 
     }
+}
+return countConsonants;
 }
