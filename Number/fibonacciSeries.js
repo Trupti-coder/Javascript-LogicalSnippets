46906d6c8653b53f8be6896a80ef0ebece49c303
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-06 09:56:26 -0500

    Update fibonacciSeries.js

--- Number/fibonacciSeries.js
+++ Number/fibonacciSeries.js
@@ -1,3 +1,9 @@
-function fibonacciSeries(){
-    
+function fibonacciSeries(n){
+    let fib=[0,1];
+    for(let i=2;i<n;i++){
+        fib[i]=fib[i-1]+fib[i-2];
+    }
+    return fib.slice(0,n);
 }
+//Example usage
+console.log(fibonacciSeries(5));
