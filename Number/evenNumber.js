e1d10cb7c86123a35a42835e15530306ef311504
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-06 09:44:35 -0500

    Update evenNumber.js

--- Number/evenNumber.js
+++ Number/evenNumber.js
@@ -1,3 +1,15 @@
-function evenNumber(){
-    
+function evenNumber(num){
+    if(num===0){
+        return false;
+    }
+
+    while(num>0){
+        if(num%2===0){
+            console.log(`Given number is even number`)
+        }
+
+        else{
+            console.log("not even number")l
+        }
+    }
 }
