c73f60bba8d8f462e15dc453c0ffd9c9a944c50e
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-06 09:52:18 -0500

    Update factorialNumber.js

--- Number/factorialNumber.js
+++ Number/factorialNumber.js
@@ -1,6 +1,6 @@
 function isFactorial(num){
-    if(num<=0){
-        return false;
+    if(num<0){
+        return "factorial is not define for negative number";
     }
    let result=1;
    for(let i=1;i<=num;i++){
@@ -8,3 +8,7 @@ function isFactorial(num){
    }
    return result;
 }
+
+console.log(isFactorial(10)); //3628800
+console.log(isFactorial(1)); //1
+console.log(isFactorial(-5)); //factorial is not define for negative number;
