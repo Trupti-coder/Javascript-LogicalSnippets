5dab3dedfe68e207ae0cb7847b74b4f55a2622d2
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-06 09:35:51 -0500

    Update decimalToBinary.js

--- Number/decimalToBinary.js
+++ Number/decimalToBinary.js
@@ -2,8 +2,13 @@ function decimalToBianry(num){
     let binary='';
     while(num>0){
         let remainder=num%2;
-        binary+=remainder+binary;
+        binary=remainder+binary;
         num=Math.floor(num/2);
     }
-
+    return binary;
 }
+
+//Example Usage
+let num=15;
+let result=decimalToBianry(num);
+console.log(result);
