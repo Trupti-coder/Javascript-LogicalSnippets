e3fab2949647a8a8b5cc4eebdc8df1cac2829404
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-06 09:49:07 -0500

    Update factorialNumber.js

--- Number/factorialNumber.js
+++ Number/factorialNumber.js
@@ -1,3 +1,10 @@
-function isFactorial(){
-    
+function isFactorial(num){
+    if(num<=0){
+        return false;
+    }
+   let result=1;
+   for(let i=1;i<=num;i++){
+    result*=i; //multiply each number from 1 to num
+   }
+   return result;
 }
