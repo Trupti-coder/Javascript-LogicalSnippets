77efb6fd90b7ca79612ad2f291069f07b7ae76b4
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-06 09:29:36 -0500

    Update binaryToDecimal.js

--- Number/binaryToDecimal.js
+++ Number/binaryToDecimal.js
@@ -2,8 +2,8 @@ function binaryToDecimal(str){
     let decimal=0
     let length=str.length;
     for(let i=0;i<length;i++){
-        let bit=parseInt(str[i]);
-        let power=length-1-i;
+        let bit=parseInt(str[i]); //Convert character to number
+        let power=length-1-i;  // calculate power of two
         decimal+=bit*Math.pow(2,power);
     }
     return decimal;
