33428a8bbc0b7d850cc7ba168ac59a6b52711c60
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-06 21:32:25 -0500

    Update findGCD.js

--- Number/findGCD.js
+++ Number/findGCD.js
@@ -1,3 +1,12 @@
-function findGCD(){
-    
+function findGCD(a,b){
+    if(a<=0) a=-a;
+    if(b<=0) b=-b;
+
+    while(b!==0){
+        let remainder=a%b;
+        a=b;
+        b=remainder;
+    }
+    return a;
+
 }
