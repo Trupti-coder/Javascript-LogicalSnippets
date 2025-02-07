a31f136a91d131d1daeacedc7ac7dc0203ea939f
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-07 11:53:19 -0500

    Update reverseNumber.js

--- Number/reverseNumber.js
+++ Number/reverseNumber.js
@@ -1,15 +1,17 @@
 function reverseNumber(num){
+    //check if number is negative return false;
     if(num<0){
         return false;
     }
     let reverse=0;
-
+    //reverse the number
     while(num>0){
-        let digit=num%10;
-        let reverse=reverse*10+digit;
-        num=num/10;
+        let digit=num%10; //get lats digit
+        let reverse=reverse*10+digit; //Add it to the reversed number
+        num=num/10; //remove the last digit
 
 
     }
+    //return reverse number
     return reverse;
 }
