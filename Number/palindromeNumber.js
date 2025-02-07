d44b5a347b866690c6bb56c3918c5055e47146d9
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-07 11:31:36 -0500

    Update palindromeNumber.js

--- Number/palindromeNumber.js
+++ Number/palindromeNumber.js
@@ -1,8 +1,12 @@
 function isPalindrome(num){
     let originalNum=num;
-    let reverseNum=0;
+    let reverse=0;
     while(num>0){
-        
+        let digit=num%10;
+        let reverse=reverse*10+digit;
+        num=Math.floor(num/10);
+
     }
+    return originalNum===reverse
 
 }
