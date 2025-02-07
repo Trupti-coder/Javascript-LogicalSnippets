3eb36724a09afdc4431fb12a3dacf4ffff06e066
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-07 11:50:12 -0500

    Update function reverseNumber.js

--- Number/reverseNumber.js
+++ Number/reverseNumber.js
@@ -1,3 +1,15 @@
 function reverseNumber(num){
-    
+    if(num<0){
+        return false;
+    }
+    let reverse=0;
+
+    while(num>0){
+        let digit=num%10;
+        let reverse=reverse*10+digit;
+        num=num/10;
+
+
+    }
+    return reverse;
 }
