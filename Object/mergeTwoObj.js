24dee4109d751db90b5c38e454e7932c80fcdaf8
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-08 10:30:26 -0500

    Update mergeTwoObj.js

--- Object/mergeTwoObj.js
+++ Object/mergeTwoObj.js
@@ -2,3 +2,10 @@ let obj1={a:1,b:2};
 let obj2={c:1,d:2};
 
 let merged={};
+for(let key in obj1){
+    merged[key]=obj1[key];
+}
+for(let key in obj2){
+    merged[key]=obj2[key];
+}
+console.log(merged);
