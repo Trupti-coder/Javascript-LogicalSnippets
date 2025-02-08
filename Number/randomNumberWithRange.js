d022321446a1d52a58eaabdcef458586e9eb21ec
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-07 21:57:37 -0500

    Update randomNumberWithRange.js

--- Number/randomNumberWithRange.js
+++ Number/randomNumberWithRange.js
@@ -3,6 +3,9 @@ function randomRange(min,max){
     let random=0;
 
     //generating Random number
-    
+    for(let i=0;i<5;i++){
+        random=(random*10+new Date().getMilliseconds()) % range;
 
+}
+return min+random;
 }
