c5e8b85ddb20f51b964957f196b17ef25d4dd38a
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-08 10:48:52 -0500

    Update function extractKeysAndValues

--- Object/storeKeyValue.js
+++ Object/storeKeyValue.js
@@ -1,3 +1,12 @@
 function extractKeysAndValues(obj){
+    let keys = [];
+    let values = [];
+
+    for (let key in obj) {
+        keys[keys.length] = key;        // Add key to keys array
+        values[values.length] = obj[key]; // Add value to values array
+    }
+
+    return [keys, values]; // Return both arrays as a pair
     
 }
