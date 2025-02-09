9cd2b241fe8f8d12dc215368c306bf5551a8cd84
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-09 14:56:08 -0500

    Update function

--- Pattern/pascalTriangle.js
+++ Pattern/pascalTriangle.js
@@ -5,6 +5,16 @@ function pascalTriangle(n){
       for (let i = 0; i < n; i++) {
         // Create a new row
         let row = [];
+
+        for (let j = 0; j <= i; j++) {
+            // The first and last elements of each row are always 1
+            if (j === 0 || j === i) {
+                row[j] = 1;
+            } else {
+                // Each triangle element is the sum of the two elements directly above it
+                row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
+            }
+        }
       }
 
 }
