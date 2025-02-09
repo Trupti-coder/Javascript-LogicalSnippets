a300d54e6c4e03abadee0e226a9e23ff45592a6d
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-09 14:54:40 -0500

    Update function

--- Pattern/pascalTriangle.js
+++ Pattern/pascalTriangle.js
@@ -1,3 +1,10 @@
 function pascalTriangle(n){
-    
+      // Initialize the triangle as an empty array
+      let triangle = [];
+
+      for (let i = 0; i < n; i++) {
+        // Create a new row
+        let row = [];
+      }
+
 }
