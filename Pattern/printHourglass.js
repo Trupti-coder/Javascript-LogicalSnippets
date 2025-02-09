0b1c2e37883d0934ee9a24606dbc644f61040bed
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-09 15:01:00 -0500

    Create printHourglass.js

---
+++ Pattern/printHourglass.js
@@ -0,0 +1,15 @@
+function printHourglass(n){
+    //top part
+
+    for (let i = n; i >= 1; i--) {
+        let line = '';
+        for (let j = 1; j <= n - i; j++) {
+            line += ' ';
+        }
+        for (let k = 1; k <= (2 * i - 1); k++) {
+            line += '*';
+        }
+        console.log(line);
+    }
+
+}
