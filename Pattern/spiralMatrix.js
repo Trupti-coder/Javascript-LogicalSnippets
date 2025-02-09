1f403a921171f4fd9c5dc1c02b3c0c4b85bb7553
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-09 14:43:57 -0500

    Update

--- Pattern/spiralMatrix.js
+++ Pattern/spiralMatrix.js
@@ -1,3 +1,7 @@
 function printSpiralMatrix(n){
-    
+    let matrix = Array.from({ length: n }, () => Array(n).fill(0));
+    let value = 1;
+    let top = 0, left = 0;
+    let bottom = n - 1, right = n - 1;
+
 }
