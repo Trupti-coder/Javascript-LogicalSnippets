25ba3558350af38857f47f7e99868de193456815
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-09 15:06:24 -0500

    Update printDiamond.js

--- Pattern/diamondPattern.js
+++ Pattern/diamondPattern.js
@@ -4,10 +4,18 @@ function printDiamond(n){
     for (let i = 1; i <= n; i++) {
         let line = '';
 
+          // Add spaces
+          for (let j = 1; j <= n - i; j++) {
+            line += ' ';
+        }
 
+        // Add stars
+        for (let k = 1; k <= 2 * i - 1; k++) {
+            line += '*';
+        }
 
-        
+        console.log(line);
+
     }
-
 
 }
