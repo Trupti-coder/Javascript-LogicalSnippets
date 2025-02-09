23150dd1127a8e308b3e96b6b9cbf725f96e62b6
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-08 21:53:16 -0500

    Update printPyramid.js

--- Pattern/printPyramid.js
+++ Pattern/printPyramid.js
@@ -6,6 +6,11 @@ function printPyramid(height){
         for(let j=1;j<=(height-i);i++){
             line+=' ';
         }
+        //Add stars
+        for(let k=1;k<=(2*i-1);k++){
+            line += '*';
+        }
+        console.log(line);
     }
 
 }
