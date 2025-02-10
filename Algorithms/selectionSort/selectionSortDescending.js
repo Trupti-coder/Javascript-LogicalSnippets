265c41b7c229b52ab71ccbe66feea34b72859d3f
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-10 10:33:10 -0500

    Update selectionSortDescending.js

--- Algorithms/selectionSort/selectionSortDescending.js
+++ Algorithms/selectionSort/selectionSortDescending.js
@@ -1,3 +1,13 @@
-function selectionSortDescending(){
-    
+function selectionSortDescending(arr){
+    let n=arr.length;
+
+    for(let i=0;i<n-1;i++){
+        let maxIndex=i;
+
+        for(let j=i+1;j<n;j++){
+            if(arr[j]>arr[maxIndex]){
+                maxIndex=j;
+            }
+        }
+    }
 }
