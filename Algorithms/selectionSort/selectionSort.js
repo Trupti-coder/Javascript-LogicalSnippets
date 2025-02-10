6cb2ef0fbe09ffff5c24a9976e91a2f0c7d2c812
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-10 10:24:21 -0500

    Update selectionSort.js

--- Algorithms/selectionSort/selectionSort.js
+++ Algorithms/selectionSort/selectionSort.js
@@ -1,3 +1,13 @@
-function selectionSort(){
-    
+function selectionSort(arr){
+
+    let n=arr.length;
+
+    for(let i=0;i<n-1;i++){
+        let minIndex=i;
+    }
+
+    for(let j=i+1;j<n;j++){
+        
+    }
+
 }
