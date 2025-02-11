63f923415ec198a5321a190810153f9015e193d0
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-10 21:44:19 -0500

    Update function selectionSortStrings

--- Algorithms/selectionSort/selectionSortStrings.js
+++ Algorithms/selectionSort/selectionSortStrings.js
@@ -1,3 +1,14 @@
-function selectionSortStrings(){
-    
+function selectionSortStrings(arr){
+    let n=arr.length;
+
+    for(let i=0;i<n-1;i++){
+        let minIndex=i;
+
+        for(let j=i+1;j<n;j++){
+            if(arr[j].toLowercase()<arr[minIndex].toLowercase()){
+                minIndex=j;
+            }
+        }
+    }
+
 }
