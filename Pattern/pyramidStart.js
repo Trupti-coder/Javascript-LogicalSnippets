1c73517e4ac10592fffa4f22dc19f924c427fab4
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-11 20:32:20 -0500

    Update printPyramid.js

--- Pattern/pyramidStart.js
+++ Pattern/pyramidStart.js
@@ -1,3 +1,12 @@
 function printPyramid(height){
-    
+    for (let i = 1; i <= height; i++) {
+        let line = '';
+
+        // Add spaces
+        for (let j = 1; j <= (height - i); j++) {
+            line += ' ';
+        }
+    }
+
+
 }
