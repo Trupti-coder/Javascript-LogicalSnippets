1b21ab58cd03ba7036cc115e7d3494cace7d4b3c
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-11 20:21:49 -0500

    Update

--- Pattern/hourGlassPattern.js
+++ Pattern/hourGlassPattern.js
@@ -6,5 +6,9 @@ function printHourglass(n){
         for(let j=1;j<=n;j++){
             line+=' ';
         }
+        for (let k = 1; k <= (2 * i - 1); k++) {
+            line += '*';
+        }
+        console.log(line);
     }
 }
