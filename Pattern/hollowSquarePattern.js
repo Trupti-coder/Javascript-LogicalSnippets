3b08474df1696cb50fb64d32d159f66cfa48f4dc
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-11 20:08:20 -0500

    Update printHollowSquare.js

--- Pattern/hollowSquarePattern.js
+++ Pattern/hollowSquarePattern.js
@@ -1,3 +1,12 @@
 function printHollowSquare(n){
-    
+    for(let i=1;i<=n;i++){
+        let line='';
+        for(let j=1;j<=n;j++){
+            if(i===1 || i===n || j===1 || j===n){
+
+            }
+    }
 }
+
+}
+
