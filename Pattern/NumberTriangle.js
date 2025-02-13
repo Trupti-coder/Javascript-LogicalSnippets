73fd039b3a385660b09e03f9c777caa29f206697
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-12 21:36:26 -0500

    Update

--- Pattern/NumberTriangle.js
+++ Pattern/NumberTriangle.js
@@ -4,8 +4,11 @@ function printNumberTriangle(rows){
         let line = '';
         // Create the numbers for the current line
         for (let j = 1; j <= i; j++){
-            
+            line += j + ' ';
+
         }
+         // Print the current line
+         console.log(line.trim());
 
     }
 }
