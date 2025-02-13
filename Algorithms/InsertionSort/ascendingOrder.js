b5033fcb342c85b01c950dc1a73e4107e786cd90
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-12 21:48:17 -0500

    Update function insertionSort

--- Algorithms/InsertionSort/ascendingOrder.js
+++ Algorithms/InsertionSort/ascendingOrder.js
@@ -4,7 +4,13 @@ function insertionSort(arr){
     for (let i = 1; i < n; i++) {
         let current = arr[i];
         let j = i - 1;
-        
+
+         // Shift elements of arr[0..i-1] that are greater than current
+         while (j >= 0 && arr[j] > current) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
     }
 
 }
