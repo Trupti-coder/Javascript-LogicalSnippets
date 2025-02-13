c9969126918923e0ae6e4e1c0cd5cebfca95fbd9
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-12 21:55:43 -0500

    Update

--- Algorithms/InsertionSort/descendingOrder.js
+++ Algorithms/InsertionSort/descendingOrder.js
@@ -1,3 +1,17 @@
 function insertionSortDescending(arr){
-    
+
+    let n = arr.length;
+
+    for (let i = 1; i < n; i++){
+
+        let current = arr[i];
+        let j = i - 1;
+
+        while (j >= 0 && arr[j] < current) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+    }
+
 }
