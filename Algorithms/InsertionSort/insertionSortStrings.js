960ec3461c235f1faa2dc5622db1ae508650ec62
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-12 22:00:49 -0500

    Update

--- Algorithms/InsertionSort/insertionSortStrings.js
+++ Algorithms/InsertionSort/insertionSortStrings.js
@@ -1,3 +1,10 @@
 function insertionSortStrings(arr){
-    
+    let n = arr.length;
+    for (let i = 1; i < n; i++){
+
+        let current = arr[i].toLowerCase();
+        let original = arr[i];
+        let j = i - 1;
+
+    }
 }
