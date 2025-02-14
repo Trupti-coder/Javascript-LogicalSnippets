dcd2ff8bdc5e3fa1cba53458eca99e9ac67e48f2
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-13 21:51:41 -0500

    Update

--- Algorithms/InsertionSort/insertionSortObjects.js
+++ Algorithms/InsertionSort/insertionSortObjects.js
@@ -4,6 +4,10 @@ function insertionSortObjects(arr,key){
     for(let i=1;i<n;i++){
         let current=arr[i];
         let j=i-1;
+
+        while (j >= 0 && arr[j][key] > current[key]) {
+            arr[j + 1] = arr[j];
+            j--;
     }
 
 }
