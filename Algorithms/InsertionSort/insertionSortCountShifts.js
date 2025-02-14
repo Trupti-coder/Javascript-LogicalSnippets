27e80b6f11b26fc5e106adb83cef931e6a70e852
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-13 22:00:57 -0500

    Update

--- Algorithms/InsertionSort/insertionSortCountShifts.js
+++ Algorithms/InsertionSort/insertionSortCountShifts.js
@@ -1,4 +1,12 @@
 function insertionSortCountShifts(arr){
-    
+
+    let n=arr.length;
+    let shiftCount=0;
+
+    for(let i=1;i<n;i++){
+        let current=arr[i];
+        let j=i-1;
+    }
+
 
 }
