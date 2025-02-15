cc2ff69df33959d3a5c25f5fcb2c556ce9c9024c
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-14 22:30:00 -0500

    Update function.js

--- Logical Snippets/function.js
+++ Logical Snippets/function.js
@@ -1,7 +1,11 @@
 function areObjectqEqual(obj1,obj2){
     return JSON.stringify(obj1)===JSON.stringify(obj2);
 
-
 }
 console.log(areObjectsEqual({ a: 1, b: 2 }, { a: 1, b: 2 }));//true
 console.log(areObjectsEqual({ a: 1, b: 2 }, { b: 2, a: 1 })); //false  because order matters
+
+
+// 2
+console.log( 1 + "2" + 3 ); // "123"
+console.log( 1 + 2 + "3" ); // "33"
