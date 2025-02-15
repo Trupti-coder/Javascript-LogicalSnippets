d0e1e79b7185f72965cd425f1c33deb93bbc2a23
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-14 22:26:19 -0500

    Update

--- Logical Snippets/function.js
+++ Logical Snippets/function.js
@@ -3,4 +3,5 @@ function areObjectqEqual(obj1,obj2){
 
 
 }
-console.log(areObjectsEqual({ a: 1, b: 2 }, { a: 1, b: 2 }));
+console.log(areObjectsEqual({ a: 1, b: 2 }, { a: 1, b: 2 }));//true
+console.log(areObjectsEqual({ a: 1, b: 2 }, { b: 2, a: 1 })); //false  because order matters
