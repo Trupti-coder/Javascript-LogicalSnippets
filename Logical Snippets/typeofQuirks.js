4081251636d9e94fffe4916aebd06cf18dbad1d7
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-15 21:52:15 -0500

    Update

---
+++ Logical Snippets/typeofQuirks.js
@@ -0,0 +1,4 @@
+console.log(typeof null); //object
+console.log(typeof NaN); //Number
+console.log(typeof []); //Object
+console.log(typeof function(){}); // function   Functions are a special type of object but return "function"
