99e8a29f89862210dfff56a290bb8e3b5f98eb71
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-15 22:03:13 -0500

    Update typeofQuirks

--- Logical Snippets/typeofQuirks.js
+++ Logical Snippets/typeofQuirks.js
@@ -1,4 +1,4 @@
-console.log(typeof null); //object
-console.log(typeof NaN); //Number
-console.log(typeof []); //Object
+console.log(typeof null); //object typeof null === "object" is a long-standing JavaScript bug.
+console.log(typeof NaN); //Number NaN is actually a number, even though it's Not-a-Number.
+console.log(typeof []); //Object Arrays ([]) are technically objects.
 console.log(typeof function(){}); // function   Functions are a special type of object but return "function"
