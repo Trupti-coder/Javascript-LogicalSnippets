4d08ef5d3af2f1e2aaeda159aa2d5025ba254fc1
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-16 14:57:11 -0500

    update

--- Logical Snippets/function.js
+++ Logical Snippets/function.js
@@ -116,7 +116,24 @@ const obj = {
     }
   };
   
-  obj.method();
+  obj.method(); //undefined
+
+  // 16
+
+  console.log("Start");
+
+setTimeout(() => {
+    console.log("Timeout");
+}, 0);
+
+Promise.resolve().then(() => {
+    console.log("Promise");
+});
+
+console.log("End");
+
+
+
   
 
 
