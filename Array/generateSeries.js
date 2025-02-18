65fd4f2c100f8771716e565a10123576c598586a
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-18 18:44:41 -0500

    Update generateSeries.js

--- Array/generateSeries.js
+++ Array/generateSeries.js
@@ -11,3 +11,7 @@ function generateSeries(){
   
 
 }
+
+//example usage
+let result=generateSeries()
+console.log(result); // // Output: [1, 2, 2, 4, 3, 6, 4, 8, 5, 10, 6, 12, 7, 14, 8, 16, 9, 18, 10, 20]
