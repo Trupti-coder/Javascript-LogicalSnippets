fd773ddd372a1d266844564dbd678badcb5c479a
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-18 18:43:29 -0500

    Update generateSeries.js

--- Array/generateSeries.js
+++ Array/generateSeries.js
@@ -3,7 +3,11 @@ function generateSeries(){
     let series=[];
     for(let i=1;i<=10;i++){
         series.push(i)  //Add the number to the series
+
+        series.push(i*2); //Add The number multiplied by 2 to the series
     }
+
+    return series; //Return the series
   
 
 }
