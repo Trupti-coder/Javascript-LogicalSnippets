7524cf349fc526f23b10cabebad205bf0348d6dc
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-17 22:49:32 -0500

    update

--- Logical Snippets/snippets.js
+++ Logical Snippets/snippets.js
@@ -4,3 +4,17 @@ let d;
 d=c;
 c.name='peter';
 console.log(d.name); //peter 
+
+// 2
+
+var x;
+var x=10;
+console.log(x);  //10
+
+// 3
+
+// var x;
+// let x=2;
+// console.log(x); //x has  been already declared
+
+
