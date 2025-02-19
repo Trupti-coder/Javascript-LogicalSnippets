914d28ec7bddba4c8045328f04a8cf62f8c76163
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-18 19:19:13 -0500

    Update  goupingArrayByCondition.js

--- Array/goupingArrayByCondition.js
+++ Array/goupingArrayByCondition.js
@@ -3,4 +3,18 @@ function groupByCondition(arr,condition){
 
     let group1=[];
     let group2=[];
+
+    // Iterate through the array
+    for(let i=0;i<arr.length;i++){
+
+        //Check the condition and push elements into the the respective groups
+
+        if(condition(arr[i])){
+            group1.push(arr[i]);
+        }
+        else{
+            group2.push(arr[i]);
+        }
+
+    }
 }
