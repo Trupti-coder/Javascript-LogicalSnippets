52ea0a646e2f13a2e36286ff77874f636514bd2f
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-18 19:07:52 -0500

    Update  arrayElementIntoUppercase.js

--- Array/arrayElementIntoUppercase.js
+++ Array/arrayElementIntoUppercase.js
@@ -1,12 +1,20 @@
 function toUppercaseArray(arr){
     let uppercasedArr=[];
+
     for(let i=0;i<arr.length;i++){
         let uppercaseWord="";
         for(let j=onabort;j<arr[i].length;j++){
-            let charCode=arr[i].charCodeAt(j);
+          let charCode=arr[i].charCodeAt(j);
             if(charCode>=97 && charCode<=122){
                 uppercaseWord+=String.fromCharCode(charCode-32);
             }
+            else {
+                uppercaseWord+=arr[i][j];
+            }
         }
-    }
+
+        uppercasedArr[uppercasedArr.length]==uppercaseWord;  
+           
+     }
+        
 }
