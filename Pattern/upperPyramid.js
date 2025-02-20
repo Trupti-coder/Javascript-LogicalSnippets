f534b6ef8c7de7ef7573aa1fbc245f7172e31204
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-20 10:11:34 -0500

    Update upperPyramid.js

--- Pattern/upperPyramid.js
+++ Pattern/upperPyramid.js
@@ -1,3 +1,10 @@
 function upperPyramid(n){
 
+    for(let i=1;i<=n;i++){
+        let str=" * ";
+        let space=" ";
+        console.log(space.repeat((n-i))+str.repeat(i*2-i));
+    }
+
 }
+upperPyramid(5);
