1e828f789655c5ebec8a8549121b276d116ec665
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-20 22:32:22 -0500

    update

--- Array/CharNumAddition.js
+++ Array/CharNumAddition.js
@@ -4,6 +4,9 @@ function numAddition(){
     for(let i=0;i<input.length;i++){
         if(input[i]>="0" && input[i]<="9"){
             let num=0;
+            while(i<input.length && input[i]>="0" && input[i] <= "9"){
+                
+            }
         }
     }
 }
