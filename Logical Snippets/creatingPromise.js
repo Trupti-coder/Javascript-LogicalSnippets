c906542f9a5585d5295d4ff7b752a3f83de16c77
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-21 11:59:49 -0500

    Update

--- Logical Snippets/creatingPromise.js
+++ Logical Snippets/creatingPromise.js
@@ -1,3 +1,12 @@
 let myPromise= new Promise((resolve,reject)=>{
-    
+    setTimeout(()=>{
+        let success=true;
+        if(success){
+            resolve("operation was successful");
+
+        }
+        else{
+            reject("operation failed..");
+        }
+    },2000);
 })
