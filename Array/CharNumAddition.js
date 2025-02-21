ccc7126ee964f82b5e54e59570dcc92f172d501d
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-20 22:34:18 -0500

    Update CharNumAddition.js

--- Array/CharNumAddition.js
+++ Array/CharNumAddition.js
@@ -5,8 +5,12 @@ function numAddition(){
         if(input[i]>="0" && input[i]<="9"){
             let num=0;
             while(i<input.length && input[i]>="0" && input[i] <= "9"){
-                
+                num=num*10+(input[i]-"0");
+                i++;
             }
+            sum += num;
+            i--;
         }
     }
+    console.log(sum);
 }
