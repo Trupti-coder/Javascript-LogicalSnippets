9e75f01e4a7cfcc6af77cc7a0fa87690e057e0f9
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-21 22:58:08 -0500

    Update randomString.js

--- String/randomString.js
+++ String/randomString.js
@@ -4,6 +4,11 @@ function generateRandomString(length){
     var randomString = '';
 
     for (var i = 0; i < length; i++){
-        
+                // Generate a random index between 0 and the length of the characters string
+                var randomIndex = Math.floor(Math.random() * characters.length);
+                // Append the character at the random index to the randomString
+                randomString += characters[randomIndex];
+
     }
+    return randomString;
 }
