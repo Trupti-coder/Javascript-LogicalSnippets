84b690d712d8094cff06f28a95368a2e7c76a534
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-22 21:39:22 -0500

    update

--- Logical Snippets/arr.js
+++ Logical Snippets/arr.js
@@ -12,4 +12,12 @@ arr.sort();
 console.log(arr);
 
 // output:[1,2,"3",4,"A","a","apple","null",undefined];
+
+
+// 3
+
+const arrays=[1, ,3];
+console.log(0 in arr);
+console.log(1 in arr);
+console.log(arr.length);
   
