71703a62f42b624213255e1ac554fc4fc39325ca
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-22 21:36:44 -0500

    Update

--- Logical Snippets/snippets.js
+++ Logical Snippets/snippets.js
@@ -339,5 +339,5 @@ let x=1;
 let y=2;
 let z=3;
 x=y=z+1;
-console.log(x,y,z); 
+console.log(x,y,z); // 4 4 3  the value of z itself is not changed by this operation. It remains 3 because the assignment only affects x and y.
 
