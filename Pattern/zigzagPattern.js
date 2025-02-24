721f0a6cb41bcb35273cbf97e787fc2495314d62
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-23 22:08:57 -0500

    Update

--- Pattern/zigzagPattern.js
+++ Pattern/zigzagPattern.js
@@ -2,6 +2,10 @@ function printZigzagPattern(rows,cols){
     let currentNumber=1;
     let result='';
     for(let i=0;i<rows;i++){
-        
+        let row=[];
+
+        for(let j=0;j<cols;j++){
+            rows.push(currentNumber++);
+        }
     }
 }
