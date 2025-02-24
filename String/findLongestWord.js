dd2a42723d7fcf2d93ba64df8ce83fc24ec1fbf4
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-23 22:49:41 -0500

    Update

--- String/findLongestWord.js
+++ String/findLongestWord.js
@@ -6,7 +6,18 @@ function findLongestWord(sentence){
 
     // Split the sentence into words
     for (let i = 0; i < sentence.length; i++){
-        
+        if (sentence[i] === ' ' || i === sentence.length - 1){
+            if (i === sentence.length - 1 && sentence[i] !== ' '){
+                word += sentence[i];
+            }
+            words.push(word);
+            word = '';
+
+        }
+        else{
+            word+=sentence[i];
+        }
+
     }
 
 }
