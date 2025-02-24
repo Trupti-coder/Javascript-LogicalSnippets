7ee1c20bd36be34eb9d0c92cfe11893f644b7014
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-23 22:36:13 -0500

    update

--- Array/topKFrequentEleOccure.js
+++ Array/topKFrequentEleOccure.js
@@ -1,3 +1,11 @@
 function topKFrequent(nums,k){
+    const frequencyMap={};
+    for(let i=0;i<nums.length;i++){
+        if(frequencyMap[nums[i]]!==undefined){
+            frequencyMap[nums[i]]++;
+        }else{
+            
+        }
+    }
 
 }
