16fef68ef059300509b8bf99473491fb281a18ab
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-23 22:20:26 -0500

    Update

--- Number/romanToInteger.js
+++ Number/romanToInteger.js
@@ -8,5 +8,16 @@ function romanToInteger(roman){
         'D':500,
         'M':1000
     };
+
+    let integer=0;
+    let previousValue=0;
+
+    for(let i=roman.length-1;i>=0;i--){
+        const currentValue=romanToIntegerMap[roman[i]];
+
+        if(currentValue<previousValue){
+            
+        }
+    }
     
 }
