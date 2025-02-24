d4d1e594e5cabffd5708eff49b8477c25d2b97ae
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-23 22:38:49 -0500

    Update

--- Array/topKFrequentEleOccure.js
+++ Array/topKFrequentEleOccure.js
@@ -4,8 +4,19 @@ function topKFrequent(nums,k){
         if(frequencyMap[nums[i]]!==undefined){
             frequencyMap[nums[i]]++;
         }else{
-            
+            frequencyMap[nums[i]]=1;
         }
     }
+
+    // Step 2: Create an array of buckets where the index represents the frequency
+    const buckets=[];
+    for(let key in frequencyMap){
+        const freq=frequencyMap[key];
+        if(buckets[freq]===undefined){
+            buckets[freq]=[];
+        }
+        buckets[freq].push(Number(key));
+    }
+
 
 }
