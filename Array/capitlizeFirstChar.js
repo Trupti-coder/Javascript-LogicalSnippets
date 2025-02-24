2c84f3e85259eccd7fe75813db6e94bfadde1a5f
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-24 12:16:57 -0500

    Update

--- Array/capitlizeFirstChar.js
+++ Array/capitlizeFirstChar.js
@@ -5,6 +5,14 @@ function capitlizeFirstChar(arr){
         let capitlizedword='';
         for(let j=0;j<word.length;j++){
             if(j===0){
+                let charCode=word.charCodeAt(j);
+                if(charCode>=97 && charCode<=122){
+                    uppercasedWords+=String.fromCharCode(charCode-32);
+
+                }
+                else{
+                    capitlizedword+=word[j];
+                }
                 
             }
 
