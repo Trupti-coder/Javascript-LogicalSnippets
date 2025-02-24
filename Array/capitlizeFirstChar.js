79f0b47615bcd50d7f4816300d6bd0a9d90a6004
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-24 12:13:47 -0500

    Update

--- Array/capitlizeFirstChar.js
+++ Array/capitlizeFirstChar.js
@@ -1,3 +1,14 @@
 function capitlizeFirstChar(arr){
+    let uppercasedArr=[];
+    for(let i=0;i<arr.lenngth;i++){
+        let word=arr[i];
+        let capitlizedword='';
+        for(let j=0;j<word.length;j++){
+            if(j===0){
+                
+            }
+
+        }
+    }
 
 }
