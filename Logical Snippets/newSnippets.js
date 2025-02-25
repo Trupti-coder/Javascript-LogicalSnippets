7bea8967fc2fd260127a8027865084e33fc17478
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-25 12:45:08 -0500

    Update

--- Logical Snippets/newSnippets.js
+++ Logical Snippets/newSnippets.js
@@ -1,3 +1,13 @@
 function checkAge(data){
-  
+    if(data==={  age : 18 }){
+        console.log('You are an adult..!')
+    }
+    else if(data=={age:18}){
+        console.log("you are still an adult")
+    }
+    else{
+        console.log('Hmm...you dont have an age i guess')
+    }
 }
+
+checkAge({age:18});
