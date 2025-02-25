b1d9b4983db97cd48adcc9f393d14f711da9f4fa
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-24 20:46:50 -0500

    Update

--- Logical Snippets/mySnippets.js
+++ Logical Snippets/mySnippets.js
@@ -33,3 +33,14 @@ const bird = {
     name: 'Mickey',
     small: true,
   };
+
+ /* 
+ 
+ A: mouse.bird.size is not valid
+B: mouse[bird.size] is not valid
+C: mouse[bird["size"]] is not valid
+D: All of them are valid 
+
+output: A: mouse.bird.size is not valid
+
+*/
