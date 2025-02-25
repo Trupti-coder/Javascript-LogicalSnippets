9f550076af4493ece24fd8afe145145eefa8aefc
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-25 12:54:17 -0500

    Update newSnippets.js

--- Logical Snippets/newSnippets.js
+++ Logical Snippets/newSnippets.js
@@ -10,4 +10,12 @@ function checkAge(data){
     }
 }
 
-checkAge({age:18});
+checkAge({age:18}); // Hmm...you dont have an age i guess
+
+/*  When testing equality, primitives are compared by their value,
+
+ while objects are compared by their reference. JavaScript checks if the objects have a reference to the same location in memory.
+
+The two objects that we are comparing don't have that: the object we passed as a parameter refers to a different location in memory than the object we used in order to check equality.
+
+This is why both { age: 18 } === { age: 18 } and { age: 18 } == { age: 18 } return false. */
