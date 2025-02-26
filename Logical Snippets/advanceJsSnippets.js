4bbda9a2f50e8b795e39cd8cea8375693d2f0a5d
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-26 11:33:14 -0500

    Update

--- Logical Snippets/advanceJsSnippets.js
+++ Logical Snippets/advanceJsSnippets.js
@@ -6,3 +6,28 @@ function sayHi(age) {
 
 console.log(sayHi.call(person, 21)); // Lydia is 21
 console.log(sayHi.bind(person, 21))// function
+
+//2 
+
+function sayHi(){
+    return(()=>0)();
+}
+console.log(typeof sayHi()); // number
+
+// 3
+// which of these values are falsy:
+0;
+new Number(0);
+('');
+(' ');
+new Boolean(false);
+undefined;
+/* output :
+0
+''
+undefined
+
+
+
+
+ */
