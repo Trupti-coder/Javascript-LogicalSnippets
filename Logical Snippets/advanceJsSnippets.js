60dadabda87d7f5a928cd8f35c8bf3675dbf656e
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-26 11:33:45 -0500

    Update

--- Logical Snippets/advanceJsSnippets.js
+++ Logical Snippets/advanceJsSnippets.js
@@ -28,6 +28,16 @@ undefined;
 undefined
 
 
+There are 8 falsy values:
 
+undefined
+null
+NaN
+false
+'' (empty string)
+0
+-0
+0n (BigInt(0))
+Function constructors, like new Number and new Boolean are truthy.
 
  */
