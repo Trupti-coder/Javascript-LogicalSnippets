9a91af5d4e22cb5a5ef33f6c72f227c0e64f597a
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-26 22:48:01 -0500

    Update

--- Logical Snippets/advanceJsSnippets.js
+++ Logical Snippets/advanceJsSnippets.js
@@ -133,3 +133,17 @@ for(const item in person){
    we set the value of item equal to the current key it’s iterating over.
     First, item is equal to name, and gets logged. Then, item is equal to age, 
     which gets logged. */
+
+    // 14
+    console.log(3+4+'5'); // "75" 
+
+    // 15
+    //what is the value of num
+    const num=parseInt('7*6',10); // 7
+    /* Only the first number in the string is returned. Based on the radix (the second argument in order to specify what type of number we want to parse it to: base 10, hexadecimal, octal, binary, etc.), the parseInt checks whether the characters in the string are valid. Once it encounters a character that isn't a valid number in the radix, it stops parsing and ignores the following characters.
+
+   * is not a valid number.
+     It only parses "7" into the decimal 7. num now holds the value of 7. */
+
+     // 16
+     
