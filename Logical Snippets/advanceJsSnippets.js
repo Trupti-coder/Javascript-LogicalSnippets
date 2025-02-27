bf7e0b0a3bc38380d2e9f736c5c9d916e778e990
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-26 22:57:51 -0500

    update

--- Logical Snippets/advanceJsSnippets.js
+++ Logical Snippets/advanceJsSnippets.js
@@ -173,4 +173,21 @@ However,
   
   console.log(person, birthYear); // output:{ name: "Lydia" }, "1997"
 
+  // 18
+
+  function greeting() {
+    throw 'Hello world!';
+  }
+  
+  function sayHi() {
+    try {
+      const data = greeting();
+      console.log('It worked!', data);
+    } catch (e) {
+      console.log('Oh no an error:', e);
+    }
+  }
+  
+  sayHi();
+
 
