1b91996e9dfb171bc5d11bbc911b7909e2b90078
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-26 22:51:49 -0500

    Update

--- Logical Snippets/advanceJsSnippets.js
+++ Logical Snippets/advanceJsSnippets.js
@@ -146,4 +146,16 @@ for(const item in person){
      It only parses "7" into the decimal 7. num now holds the value of 7. */
 
      // 16
-     
+     [1, 2, 3].map(num => {
+        if (typeof num === 'number') return;
+        return num * 2;
+      }); 
+      
+      /* output /;[undefined, undefined, undefined] 
+      When mapping over the array, the value of num is equal to the element it’s currently looping over. 
+      In this case, the elements are numbers, so the condition of the if statement typeof num === "number" returns true. The map function creates a new array and inserts the values returned from the function.
+
+However,
+ we don’t return a value.
+  When we don’t return a value from the function, the function returns undefined. For every element in the array, the function block gets called, so for each element we return undefined. 
+
