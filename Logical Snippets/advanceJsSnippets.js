7fbd0c3c8076a827912d932be58baf5e1a0af6a7
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-26 22:59:17 -0500

    Update

--- Logical Snippets/advanceJsSnippets.js
+++ Logical Snippets/advanceJsSnippets.js
@@ -192,4 +192,10 @@ However,
 
   // output : Oh no an error: Hello world!
 
+  /*   
+  With the throw statement, we can create custom errors. With this statement, you can throw exceptions. An exception can be a string, a number, a boolean or an object. In this case, our exception is the string 'Hello world!'.
+
+With the catch statement, we can specify what to do if an exception is thrown in the try block. An exception is thrown: the string 'Hello world!'. e is now equal to that string, which we log. This results in 'Oh an error: Hello world!'.
+        */
+
 
