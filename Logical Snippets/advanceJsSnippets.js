c36605f80d23164e5ea0dd688cc8d59f41eb0a2a
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-27 21:20:41 -0500

    Update

--- Logical Snippets/advanceJsSnippets.js
+++ Logical Snippets/advanceJsSnippets.js
@@ -277,3 +277,9 @@ you can use bracket notation. The first character in the string has index 0,
 
 sum(10); // 20
 
+/* You can set a default parameter's value equal to another parameter of the function,
+ as long as they've been defined before the default parameter.
+  We pass the value 10 to the sum function. If the sum function only receives 1 argument,
+   it means that the value for num2 is not passed, and the value of num1 is equal to the passed value 10 in this case.
+    The default value of num2 is the value of num1, which is 10.
+ num1 + num2 returns 20.   */
