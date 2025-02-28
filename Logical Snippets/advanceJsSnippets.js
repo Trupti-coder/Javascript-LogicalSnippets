644353d9eaaf44897e438da5acd789532933672c
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-28 11:47:36 -0500

    Update

--- Logical Snippets/advanceJsSnippets.js
+++ Logical Snippets/advanceJsSnippets.js
@@ -375,4 +375,8 @@ SyntaxErrors get thrown when you've written something that isn't valid JavaScrip
 const output = `${[] && 'Im'}possible!
 You should${'' && `n't`} see a therapist after so much JavaScript lol`;
 
+/*[] is a truthy value. With the && operator, the right-hand value will be returned if the left-hand value is a truthy value. In this case, the left-hand value [] is a truthy value, so "Im' gets returned.
+
+"" is a falsy value. If the left-hand value is falsy, nothing gets returned. n't doesn't get returned. */
+
 
