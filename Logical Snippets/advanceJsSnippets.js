784bd65b51a983047e66f1d9da36f18df803b7be
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-28 12:03:23 -0500

    Update

--- Logical Snippets/advanceJsSnippets.js
+++ Logical Snippets/advanceJsSnippets.js
@@ -430,6 +430,10 @@ for (let item of set) {
 Promise.resolve(5);
 // output:  Promise {<fulfilled>: 5}
 
-/*  */
+/* We can pass any type of value we want to Promise.
+resolve, either a promise or a non-promise. The method itself returns a promise with the resolved value (<fulfilled>). If you pass a regular function, it'll be a resolved promise with a regular value. If you pass a promise, it'll be a resolved promise with the resolved value of that passed promise.
+
+In this case, we just passed the numerical value 5.
+ It returns a resolved promise with the value 5.  */
 
 
