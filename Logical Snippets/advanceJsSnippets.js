4e2cf3b0eb0bff3a9f1c39010665e861ed9c1885
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-27 22:27:50 -0500

    Update

--- Logical Snippets/advanceJsSnippets.js
+++ Logical Snippets/advanceJsSnippets.js
@@ -336,6 +336,16 @@ getItems(["banana", "apple"], "pear", "orange")
 
 // 31
 
+function nums(a, b) {
+  if (a > b) console.log('a is bigger');
+  else console.log('b is bigger');
+  return
+  a + b;
+}
+
+console.log(nums(4, 2));
+console.log(nums(1, 2));
+
 
 
 
