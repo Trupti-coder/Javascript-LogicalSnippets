36e1ad2aa2e509f9831f6b53dd315a53c9a39328
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-03-01 13:21:52 -0500

    update

--- String/firstNonRepeatingChar.js
+++ String/firstNonRepeatingChar.js
@@ -1,6 +1,16 @@
 function firstNonRepeatingChar(str){
     let frequency={};
     for(let i=0;i<str.length;i++){
+        let char=str[i];
+        if(frequency[char]){
+            frequency[char]++;
+        }
+        else{
+            frequency[char]=1
+        }
+    }
+
+    for(let i=0;i<str.length;i++){
         
     }
 }
