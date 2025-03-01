d00715ac685ed1871507c2b1599c681ba8c3f6f2
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-03-01 13:30:34 -0500

    Update

--- String/generatePermutation.js
+++ String/generatePermutation.js
@@ -2,6 +2,14 @@ function permute(str){
     let result=[];
 
     function generatePermutation(current,remaining){
+        if(remaining.length===0){
+            result.push(current);
+        }
+        else{
+            for(let i=0;i<remaining.length;i++){
+                
+            }
+        }
 
     }
 }
