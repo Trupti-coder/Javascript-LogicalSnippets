f850966a4b620a8036c9dcb47d971b7ed2591b9e
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-03-01 13:48:25 -0500

    Update

--- String/longestPalindromicSubStr.js
+++ String/longestPalindromicSubStr.js
@@ -1,3 +1,13 @@
 function longestPalindromicSubString(str){
-    
+
+    let n=str.length;
+    let maxLength=1;
+    let start=0;
+
+    let table=Array(n).fill(0).map(()=>Array(n).fill(false));
+
+    for(let i=0;i<n;i++){
+        table[i][i]=true;
+    }
+
 }
