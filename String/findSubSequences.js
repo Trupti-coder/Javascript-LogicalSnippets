c4824f6e3b9f259d5af326803746c148db2b6658
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-03-01 14:00:09 -0500

    Update

--- String/findSubSequences.js
+++ String/findSubSequences.js
@@ -2,6 +2,12 @@ function findAllSubSequences(str){
     let result=[];
 
     function generateSubSequences(current,index){
-        
+        if(index===str.length){
+            if(current!==""){
+                result.push(current);
+            }
+            return;
+        }
+
     }
 }
