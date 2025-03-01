e3866824248fcf5826f0438e6d25255b90b5fcef
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-02-28 21:18:02 -0500

    Update

--- String/anagramGroup.js
+++ String/anagramGroup.js
@@ -4,9 +4,16 @@ function groupAnagrams(words){
         let chars=word.split('');
         for(let i=0;i<chars.length;i++){
             for(let j=0;j<chars.length-1-i;j++){
-                
+                if(chars[j]>chars[j+1]){
+                    let temp=chars[j];
+                    chars[j]=chars[j+1];
+                    chars[j+1]=temp;
+                }
+
             }
         }
+
+        return chars.join('');
 
     }
 
