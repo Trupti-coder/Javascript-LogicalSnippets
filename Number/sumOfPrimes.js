3c6014334bc0773ca009a02b09b9b925d5b15a42
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-03-02 11:58:35 -0500

    Update

--- Number/sumOfPrimes.js
+++ Number/sumOfPrimes.js
@@ -1,3 +1,12 @@
 function isPrime(num){
-    
+    if(num<=1){
+        return false;
+    }
+
+    for(let i=2;i<num;i++){
+        if(num%i===0){
+            return false;
+        }
+        return true;
+    }
 }
