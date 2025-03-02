503b7daa6cd7bc131c08b8d8952d4860991763b7
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-03-02 11:22:37 -0500

    Update

--- String/isomorphicString.js
+++ String/isomorphicString.js
@@ -2,4 +2,12 @@ function isIsomorphic(str1,str2){
     if(str1.length!==str2.length){
         return false;
     }
+
+    const mapStr1={};
+    const mapStr2={};
+
+    for(let i=0;i<str1.length;i++){
+        const charStr1=str[i];
+        const charStr2=str2[i];
+    }
 }
