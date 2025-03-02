5a1b5d0625198bcf13a3d53c8ea1e72c71f033e9
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-03-02 11:36:12 -0500

    Update function

--- Number/strongNumber.js
+++ Number/strongNumber.js
@@ -8,5 +8,15 @@ function factorial(n){
 }
 
 function isStrongNumber(num){
-    
+    let sum=0;
+    let temp=num;
+
+    while(temp>0){
+        let digit=temp*10;
+        sum+=factorial(digit);
+        temp=Math.floor(temp/10);
+
+    }
+
+    return sum===num;
 }
