ecc46a614b44d23bce26ed8b7d2365a1bb135da3
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-03-02 12:06:50 -0500

    Update

--- Number/digitalRoot.js
+++ Number/digitalRoot.js
@@ -2,7 +2,12 @@ function digitalRoot(num){
     while(num>=0){
         let sum=0;
         while(num>0){
-            
+            let digit=num%10;
+            sum+=digit;
+            num=Math.floor(num/10);
+
         }
+        num=sum,;
     }
+    return num;
 }
