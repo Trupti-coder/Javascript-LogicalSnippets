1d206d29b51e5cfe2879cca05d3cbac755e631b3
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-03-03 19:49:13 -0500

    Update

--- Number/findSmallestPalindrome.js
+++ Number/findSmallestPalindrome.js
@@ -19,5 +19,12 @@ function findLargestPalindrome(N){
 }
 
 function isPalindrome(){
-    
+    let str=num.toString();
+    let len=str.length;
+    for(let i=0;i<len/2;i++){
+        if(str[i]!==str[len-1-i]){
+            return false;
+        }
+    }
+    return true;
 }
