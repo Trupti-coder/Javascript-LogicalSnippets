a90010a6bd4b9b5d82db728d18f35be7c4d77bae
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-03-03 19:44:21 -0500

    Update

--- Number/findSmallestPalindrome.js
+++ Number/findSmallestPalindrome.js
@@ -1,3 +1,11 @@
-function findSmallestPalindrome(){
-    
+function findSmallestPalindrome(N){
+    if(N===1){
+        return 1;
+    }
+
+    let num=Math.pow(10,N-1);
+    while(!isPalindrome(num)){
+        num++;
+    }
+    return num;
 }
