70e4bb263e17c184adf5d2ce9e05e46e66632424
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-03-03 21:14:08 -0500

    Update

--- Array/findUniqueElement.js
+++ Array/findUniqueElement.js
@@ -1,6 +1,12 @@
 function findUniqueElement(arr){
     let xor=0;
     for(let i=0;i<arr.length;i++){
-        
+        xor ^= arr[i];
     }
+
+      // Find the rightmost set bit (bit that is different between the two unique elements)
+      let setBit = 1;
+      while ((xor & setBit) === 0) {
+          setBit <<= 1;
+      }
 }
