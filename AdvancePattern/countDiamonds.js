d8d5d46731867de9835f8b0a51933fdb54c2e6cd
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-03-15 21:19:22 -0400

    Update

--- AdvancePattern/countDiamonds.js
+++ AdvancePattern/countDiamonds.js
@@ -5,7 +5,16 @@ function countDiamonds(grid){
 
     for(let i=1;i<n-1;i++){
         for(let j=1;j<n-1;j++){
-            
+            let size=1;
+            while(
+
+                i - size >= 0 && i + size < n && j - size >= 0 && j + size < n &&
+                grid[i - size][j] === 1 && grid[i + size][j] === 1 &&
+                grid[i][j - size] === 1 && grid[i][j + size] === 1
+              
+            ){
+                
+            }
         }
     }
 
