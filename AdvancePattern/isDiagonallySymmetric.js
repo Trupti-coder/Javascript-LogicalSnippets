8d4893550186cd8abb789100003e388c73c10706
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-03-15 21:26:43 -0400

    Update isDiagonallySymmetric.js

--- AdvancePattern/isDiagonallySymmetric.js
+++ AdvancePattern/isDiagonallySymmetric.js
@@ -3,8 +3,12 @@ function isDiagonallySymmetric(grid){
     let n=grid.length;
     for(let i=0;i<n;i++){
         for(let j=0;j<i;j++){
-            
+            if(grid[i][j]!==grid[j][i]){
+                return false;
+            }
         }
     }
+
+    return true;
 
 }
