948155b41a8be54835be9af921deec4eeb997968
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-03-15 21:31:39 -0400

    Update

--- AdvancePattern/countSymmetryWays.js
+++ AdvancePattern/countSymmetryWays.js
@@ -1,3 +1,12 @@
 function countSymmetryWays(pattern){
-    
+    let n=pattern.length;
+    let ways=1;
+
+    for(let i=0;i<n;i++){
+        for(let j=0;j<i;j++){
+            if(pattern[i][j] === '*' || pattern[j][i] === '*'){
+                
+            }
+        }
+    }
 }
