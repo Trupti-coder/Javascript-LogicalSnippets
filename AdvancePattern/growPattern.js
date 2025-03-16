9ef790efdaae4a9936d7e0fc2fe9b82922383c4f
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-03-15 21:44:21 -0400

    Update

--- AdvancePattern/growPattern.js
+++ AdvancePattern/growPattern.js
@@ -8,7 +8,12 @@ function growPattern(grid,t){
         for(let i=0;i<n;i++){
             for(let j=0;j<n;j++){
                 if(grid[i][j]===1){
-                    
+
+                    if (i > 0) temp[i - 1][j] = 1;
+                    if (i < n - 1) temp[i + 1][j] = 1;
+                    if (j > 0) temp[i][j - 1] = 1;
+                    if (j < n - 1) temp[i][j + 1] = 1;
+
                 }
             }
         }
