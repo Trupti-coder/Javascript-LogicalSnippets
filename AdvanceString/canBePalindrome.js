d1f07befdd11af426e19f30dbe017658ed1474e1
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-03-16 12:15:17 -0400

    Update

--- AdvanceString/canBePalindrome.js
+++ AdvanceString/canBePalindrome.js
@@ -1,3 +1,11 @@
 function canBePalindrome(word){
-    
+
+    let freq={}
+    for(let i=0;i<word.length;i++){
+        let char=word[i];
+        freq[char]=(freq[char] || 0)+1;
+    }
+
+    let oddCount=0;
+
 }
