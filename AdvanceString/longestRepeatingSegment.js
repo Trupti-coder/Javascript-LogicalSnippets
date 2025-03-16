572cef365ca9ee778f07a846382eee927d55e4d5
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-03-16 12:39:18 -0400

    Update

--- AdvanceString/longestRepeatingSegment.js
+++ AdvanceString/longestRepeatingSegment.js
@@ -4,5 +4,15 @@ function longestRepeatingSegment(str){
     let maxLen = 0;
     let longestSegment = "";
 
+    for (let len = 1; len <= n; len++) {
+        for (let i = 0; i + len <= n; i++) {
+
+            let sub = str.substring(i, i + len);
+            let count = 0;
+
+
+        }
+    }
+
 
 }
