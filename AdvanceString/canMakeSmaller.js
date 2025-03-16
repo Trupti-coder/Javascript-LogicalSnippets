41717922f2a963148fbca668c425caa746634fcc
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-03-16 12:24:34 -0400

    Update

--- AdvanceString/canMakeSmaller.js
+++ AdvanceString/canMakeSmaller.js
@@ -1,3 +1,12 @@
 function canmakeSmaller(str){
-    
+    let n=str.length;
+    for(let i=0;i<n;i++){
+        for(let j=i+1;j<n;j++){
+            let temp=str.split(' ');
+            let t=temp[i];
+            temp[i]=temp[j];
+            temp[j]=t;
+            
+        }
+    }
 }
