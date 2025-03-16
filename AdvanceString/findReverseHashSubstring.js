de9c66cf840086e3821955a65c7f78db82d8c988
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-03-16 12:33:08 -0400

    Update

--- AdvanceString/findReverseHashSubstring.js
+++ AdvanceString/findReverseHashSubstring.js
@@ -3,7 +3,16 @@ function findReversedHashSubstring(str){
     for(let len=1;len<=n;len++){
         for(let i=0;i+len<=n;i++){
             let reversed="";
-            
+
+            for(let k=Sub.length-1;j>=0;j--){
+                reversed+=sub[j];
+
+            }
+
+            for(let k=0;k+len<=n;k++){
+                
+            }
+
         }
     }
 }
