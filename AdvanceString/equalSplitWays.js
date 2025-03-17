8d41ea4d22846fea87ce4479973c3175fd354c82
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-03-16 22:10:18 -0400

    Update

--- AdvanceString/equalSplitWays.js
+++ AdvanceString/equalSplitWays.js
@@ -1,3 +1,10 @@
 function equalSplitWays(str){
-    
+    let n=str.length;
+    if(n%2!==0)return 0;
+    let mid=n/2;
+    let freq1={},freq2={};
+    for(let i=0;i<mid;i++){
+        
+    }
+
 }
