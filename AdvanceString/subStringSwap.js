fb9efd2529d7a24181a328a80da09b36f5cb04e9
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-03-17 22:11:36 -0400

    Update

--- AdvanceString/subStringSwap.js
+++ AdvanceString/subStringSwap.js
@@ -8,6 +8,14 @@ function subStringSwap(str){
         
    for (let j = i + 1; j < n; j++){ 
 
+    // Swap characters
+    let swapped = "";
+    for (let k = 0; k < n; k++) {
+        if (k === i) swapped += str[j];
+        else if (k === j) swapped += str[i];
+        else swapped += str[k];
+    }
+
 
      
 }
