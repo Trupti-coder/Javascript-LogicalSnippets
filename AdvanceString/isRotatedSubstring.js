27ccd3be4b418dfb1ca1a5ea723cae3334eca6f5
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-03-17 22:15:31 -0400

    Update

--- AdvanceString/isRotatedSubstring.js
+++ AdvanceString/isRotatedSubstring.js
@@ -1,3 +1,16 @@
 function isRotatedSubstring(s1,s2){
-    
+
+    if (s1.length !== s2.length) return false;
+
+    let n = s1.length;
+    for (let i = 0; i < n; i++){
+
+        let match = true;
+        for (let j = 0; j < n; j++){
+
+            
+        }
+
+    }
+
 }
