9ce29b069ef7ae596d18ad432f6a5f53a903a70a
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-03-18 10:32:45 -0400

    Update

--- AdvancePattern/isMagicSquare.js
+++ AdvancePattern/isMagicSquare.js
@@ -7,5 +7,14 @@ function isMagicSquare(matrix){
     
     for (let i = 0; i < n; i++) sum += matrix[0][i]; // First row sum
 
-    
+     // Check rows and columns
+     for (let i = 0; i < n; i++) {
+        let rowSum = 0;
+        let colSum = 0;
+        for (let j = 0; j < n; j++) {
+            rowSum += matrix[i][j];
+            colSum += matrix[j][i];
+        }
+
+
 }
