28c3b608e60716797c46fdec7cd3c6ca5ce51e08
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-03-17 22:11:05 -0400

    Update

--- AdvanceString/subStringSwap.js
+++ AdvanceString/subStringSwap.js
@@ -1,3 +1,17 @@
 function subStringSwap(str){
-    
+
+    let n = str.length;
+    let maxLen = 0;
+
+    for (let i = 0; i < n; i++){
+
+        
+   for (let j = i + 1; j < n; j++){ 
+
+
+     
+}
+
+    }
+
 }
