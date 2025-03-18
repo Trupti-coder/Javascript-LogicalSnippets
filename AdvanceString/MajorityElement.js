ce9892eacda1a76b1014e7e4441c2c3cbd70bf6c
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-03-18 10:38:05 -0400

    Update

--- AdvanceString/MajorityElement.js
+++ AdvanceString/MajorityElement.js
@@ -5,7 +5,20 @@ function majorityElement(arr){
 
     // Step 1: Find a candidate using Boyer-Moore Voting Algorithm
     for (let i = 0; i < arr.length; i++){
-        
+
+        if (count === 0) {
+            candidate = arr[i];
+            count = 1;
+
+
+        } else if (arr[i] === candidate) {
+            count++;
+        } else {
+            count--;
+        }
+
+
+
     }
 
 }
