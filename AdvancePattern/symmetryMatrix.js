f6d334941e487a0b87259b097fb2a133e8273d2d
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-03-18 10:47:24 -0400

    Update

--- AdvancePattern/symmetryMatrix.js
+++ AdvancePattern/symmetryMatrix.js
@@ -14,5 +14,14 @@ function  isHorizontallySymmetric(matrix){
 }
 
 function isVerticallySymmetric(matrix){
-    
+
+    let n = matrix.length;
+    for (let i = 0; i < n; i++){
+
+        for (let j = 0; j < n / 2; j++){
+            
+        }
+
+    }
+
 }
