b07ed7a2a172754c88f3ed7c6f296a0d5a83b040
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-03-18 10:29:23 -0400

    Update

--- AdvancePattern/spiralPrint.js
+++ AdvancePattern/spiralPrint.js
@@ -10,9 +10,14 @@ function spiralPrint(matrix){
 
         for (let i = left; i <= right; i++) result.push(matrix[top][i]);
         top++;
-        
+
         for (let i = top; i <= bottom; i++) result.push(matrix[i][right]);
         right--;
+
+        if (top <= bottom) {
+            for (let i = right; i >= left; i--) result.push(matrix[bottom][i]);
+            bottom--;
+        }
 
     }
 
