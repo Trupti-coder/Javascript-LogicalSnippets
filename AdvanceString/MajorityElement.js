fd0514bc67ea8396a9f98818a5aa0883eabd335e
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-03-18 10:38:39 -0400

    Update

--- AdvanceString/MajorityElement.js
+++ AdvanceString/MajorityElement.js
@@ -21,4 +21,12 @@ function majorityElement(arr){
 
     }
 
+     // Step 2: Validate the candidate
+     count = 0;
+     for (let i = 0; i < arr.length; i++) {
+         if (arr[i] === candidate) count++;
+     }
+ 
+     return count > arr.length / 2 ? candidate : -1;
+
 }
