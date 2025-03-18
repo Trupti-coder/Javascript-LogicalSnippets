5f8875da9d470e1e3dbb1f9ee105b7585b4555c7
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-03-17 22:04:01 -0400

    Update

--- AdvanceString/minDeletionsForUniqueSubstrings.js
+++ AdvanceString/minDeletionsForUniqueSubstrings.js
@@ -1,3 +1,16 @@
 function minDeletionsForUniqueSubstrings(str){
-    
+
+    let n = str.length;
+    let minDeletions = 0;
+
+    for (let len = 1; len < n; len++){
+
+        for (let i = 0; i <= n - len; i++){
+
+            
+        }
+
+    }
+
+
 }
