2cb198f7934b4ab80b770d3a490f008d63ecf744
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-03-19 19:29:44 -0400

    Update

--- AdvancePattern/isXPattern.js
+++ AdvancePattern/isXPattern.js
@@ -1,3 +1,26 @@
 function isXPattern(matrix){
-    
+
+    let n = matrix.length;
+    for (let i = 0; i < n; i++){
+
+        for (let j = 0; j < n; j++) {
+
+
+            if (i === j || i + j === n - 1) {
+                if (matrix[i][j] !== 1) return false;
+            
+            }
+
+            else{
+
+
+                
+            }
+
+
+
+        }
+
+    }
+
 }
