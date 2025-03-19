ce8582a300bbe5fe6929a008b6ac6c8c8b755776
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-03-19 19:20:42 -0400

    update

--- AdvancePattern/isValidSpiral.js
+++ AdvancePattern/isValidSpiral.js
@@ -5,4 +5,9 @@ function isValidSpiral(matrix){
     let count = 1;
     let top = 0, left = 0, bottom = n - 1, right = n - 1;
 
+    while (top <= bottom && left <= right) {
+        for (let i = left; i <= right; i++) {
+            if (matrix[top][i] !== count++) return false;
+        }
+
 }
