7be617e140165aef4a38dcea87f1e50a51e190d1
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-03-19 19:34:11 -0400

    Update

--- AdvancePattern/oneBorder.js
+++ AdvancePattern/oneBorder.js
@@ -5,7 +5,9 @@ function oneBorder(matrix){
 
     // Check top and bottom rows
     for (let i = 0; i < m; i++){
-        
+
+        if (matrix[0][i] !== 1 || matrix[n - 1][i] !== 1) return false;
+
     }
 
 }
