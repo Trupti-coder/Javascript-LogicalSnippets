32616c494dfbcde57207bf126459491a12861c5f
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-03-19 19:25:46 -0400

    update

--- AdvancePattern/isDiagonallyDominant.js
+++ AdvancePattern/isDiagonallyDominant.js
@@ -2,7 +2,13 @@ function isDiagonallyDominant(matrix){
 
     let n = matrix.length;
     for (let i = 0; i < n; i++){
-        
+
+        let diagonal = Math.abs(matrix[i][i]);
+        let rowSum = 0;
+        for (let j = 0; j < n; j++) {
+            
+        }
+
     }
 
 }
