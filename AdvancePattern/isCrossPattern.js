5cf28df8aa48ee2cef937c3db8a3e9dcd61c7b52
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-03-20 22:15:09 -0400

    Update

--- AdvancePattern/isCrossPattern.js
+++ AdvancePattern/isCrossPattern.js
@@ -7,7 +7,23 @@ function isCrossPattern(matrix){
 
     for (let i = 0; i < n; i++) {
 
-        
+
+        for (let j = 0; j < n; j++){
+
+
+            if (i === mid || j === mid) {
+                if (matrix[i][j] !== 1) return false;
+            } 
+
+            else{
+
+                
+            }
+
+
+        }
+
+
     }
 
 
