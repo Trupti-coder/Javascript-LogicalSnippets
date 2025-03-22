046184bc9008a62df0611f1202e9b6dd7f3c4562
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-03-21 20:36:35 -0400

    Update

--- AdvancePattern/hasDiagonalZeros.js
+++ AdvancePattern/hasDiagonalZeros.js
@@ -15,3 +15,11 @@ function hasDiagonalZeros(matrix){
     return true;
 
 }
+
+
+let zeroDiagonalMatrix = [
+    [0, 1, 0],
+    [2, 0, 3],
+    [0, 4, 0]
+];
+console.log(hasDiagonalZeros(zeroDiagonalMatrix)); // true
