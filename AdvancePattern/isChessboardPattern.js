c332fd6c1bba3c7ca73ff99501e5e504dbe28f9d
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-03-22 19:25:33 -0400

    Update

--- AdvancePattern/isChessboardPattern.js
+++ AdvancePattern/isChessboardPattern.js
@@ -1,3 +1,15 @@
 function isChessboardPattern(matrix){
-    
+
+
+    let n = matrix.length;
+
+    for (let i = 0; i < n; i++){
+
+
+        for (let j = 0; j < n; j++){
+
+            
+        }
+    }
+
 }
