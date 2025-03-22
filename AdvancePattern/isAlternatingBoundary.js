24b98b3e8a477b95e5d85c7757c704e6d1995d1d
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-03-22 19:17:57 -0400

    Update

--- AdvancePattern/isAlternatingBoundary.js
+++ AdvancePattern/isAlternatingBoundary.js
@@ -1,3 +1,14 @@
 function isAlternatingBoundary(matrix){
-    
+
+
+    let n = matrix.length;
+    let current = matrix[0][0]; // Start with the first cell
+
+    for (let i = 0; i < n; i++) {
+
+
+
+        
+    }
+
 }
