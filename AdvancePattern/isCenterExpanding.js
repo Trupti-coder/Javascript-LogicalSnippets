65b1cc98e859309cd271c2d9e4eda206a90e7d0b
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-03-21 20:39:23 -0400

    update

--- AdvancePattern/isCenterExpanding.js
+++ AdvancePattern/isCenterExpanding.js
@@ -10,13 +10,18 @@ function isCenterExpanding(matrix){
         for (let j = 0; j <= mid; j++){
             if(
 
+                matrix[i][j] !== matrix[n - i - 1][j] ||
+                matrix[i][j] !== matrix[i][n - j - 1] ||
+                matrix[i][j] !== matrix[n - i - 1][n - j - 1]
 
 
 
 
+
+            ){
 
                 
-            )
+            }
         }
     }
 
