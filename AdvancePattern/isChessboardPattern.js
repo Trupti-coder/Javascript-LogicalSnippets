22ae0180ca38ca2bc9cd8c6c566ee081a89dea43
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-03-22 19:26:02 -0400

    Up[daete

--- AdvancePattern/isChessboardPattern.js
+++ AdvancePattern/isChessboardPattern.js
@@ -8,7 +8,13 @@ function isChessboardPattern(matrix){
 
         for (let j = 0; j < n; j++){
 
-            
+
+            let expected = (i + j) % 2; // 0 for even sum, 1 for odd sum
+            if (matrix[i][j] !== expected) {
+                return false;
+            }
+
+
         }
     }
 
