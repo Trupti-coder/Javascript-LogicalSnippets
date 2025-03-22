f354bb2e4042ee04231860fdbb54d0b786a2458a
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-03-22 19:23:46 -0400

    Update

--- AdvancePattern/isZigZag.js
+++ AdvancePattern/isZigZag.js
@@ -29,6 +29,16 @@ function isZigZag(matrix){
 
     }
 
+    return true;
+
 
 
 }
+
+
+let zigZagMatrix = [
+    [1, 2, 3],
+    [6, 5, 4],
+    [7, 8, 9]
+];
+console.log(isZigZag(zigZagMatrix)); // true
