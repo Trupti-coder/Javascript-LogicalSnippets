c9f210d302f55622bff3070bb7fd54b5f5ffcf57
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-03-23 14:47:09 -0400

    Update

--- AdvancePattern/diagonalDifference.js
+++ AdvancePattern/diagonalDifference.js
@@ -4,4 +4,11 @@ function diagonalDifference(matrix){
     let mainDiagonalSum = 0;
     let secondaryDiagonalSum = 0;
 
+    for (let i = 0; i < n; i++) {
+        mainDiagonalSum += matrix[i][i];
+        secondaryDiagonalSum += matrix[i][n - i - 1];
+
+
+    }
+
 }
