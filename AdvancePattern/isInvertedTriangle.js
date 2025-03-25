47935749f2f7b6a49d2c404ba2e905ca483153b0
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-03-24 23:34:39 -0400

    Update

--- AdvancePattern/isInvertedTriangle.js
+++ AdvancePattern/isInvertedTriangle.js
@@ -4,7 +4,14 @@ function isInvertedTriangle(matrix){
     let n = matrix.length;
 
     for (let i = 0; i < n; i++) {
-        for (let j = i; j < n; j++)
+        for (let j = i; j < n; j++){
+
+
+            if (matrix[i][j] !== 1) return false;
+
+        }
 
 }
+
+return true;
 }
