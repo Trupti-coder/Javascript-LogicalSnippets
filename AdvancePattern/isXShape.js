6d311111e561d20ba127dd3e2f13f2c278ad28ae
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-03-24 23:31:20 -0400

    Update

--- AdvancePattern/isXShape.js
+++ AdvancePattern/isXShape.js
@@ -4,7 +4,14 @@ function isXShape(matrix){
 
     for (let i = 0; i < n; i++){
 
-        
+        for (let j = 0; j < n; j++){
+
+            if (i === j || i + j === n - 1) {
+                if (matrix[i][j] !== 1) return false;
+            } else {
+                if (matrix[i][j] !== 0) return false;
+            }
+        }
     }
 
 
