a34d1058bc6f8c526c4c018135dfb3164d64974a
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-03-24 23:46:24 -0400

    update

--- AdvancePattern/snakePattern.js
+++ AdvancePattern/snakePattern.js
@@ -1,4 +1,14 @@
 function snakePattern(matrix){
 
-    
+    let n = matrix.length;
+    let result = [];
+
+    for (let i = 0; i < n; i++){
+
+        if (i % 2 === 0) {
+            for (let j = 0; j < n; j++){
+
+                
+            }
+    }
 }
